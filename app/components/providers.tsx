f9041103b4afe3ac44acc33cab5f06aa900c9977
0xshikhar <64684053+0xshikhar@users.noreply.github.com>
2026-05-03 23:21:16 +0530

    chore(deps): Configure React Query provider

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -2,17 +2,22 @@
 
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClusterProvider } from "./cluster-context";
 import { WalletProvider } from "../lib/wallet/context";
 import { SolanaClientProvider } from "../lib/solana-client-context";
 
 export function Providers({ children }: PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
       <ClusterProvider>
         <SolanaClientProvider>
-          <WalletProvider>{children}</WalletProvider>
+          <QueryClientProvider client={queryClient}>
+            <WalletProvider>{children}</WalletProvider>
+          </QueryClientProvider>
         </SolanaClientProvider>
         <Toaster position="bottom-right" richColors />
       </ClusterProvider>
